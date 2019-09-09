4e1f9f95ab324399e74dfcf50c441990c8cbc720
misheki <kurky17@yahoo.com>
2019-09-08 23:06:08 -0700

    upgraded laravel to 6.0

---
+++ database/migrations/2019_07_13_012627_create_organizations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('org_category_id');
+            $table->foreign('org_category_id')->references('id')->on('org_categories');
+            $table->string('name');
+            $table->string('code');
+            $table->string('contact_person');
+            $table->string('email');
+            $table->string('address');
+            $table->string('website'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizations');
+    }
+}
---
+++ database/migrations/2019_07_13_012922_create_workers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('organization_id');
+            $table->foreign('organization_id')->references('id')->on('organizations');
+            $table->string('worker_code')->unique(); //system generated ie AA12345
+            $table->string('name');
+            $table->string('code'); //short or nick name
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('');
+            $table->date('started_at')->nullable();
+            $table->string('staff_id')->nullable();
+            $table->string('qr'); // OWLY#<ORG CODE>#<WORKER CODE>
+            $table->string('status'); // A for ACTIVE, I for INACTIVE
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workers');
+    }
+}
