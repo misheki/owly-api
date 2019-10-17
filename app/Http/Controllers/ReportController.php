6e4f6a8f7b0da297b8c8bed2d0d08052ce022505
misheki <kurky17@yahoo.com>
2019-10-17 12:51:24 -0700

    Test daily report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -44,7 +44,6 @@ class ReportController extends Controller
                         if(!in_array($scan->worker_code, array_column($data, 'worker_code'))){
                             array_push($data, array("id"=>$count, "worker_code"=>$scan->worker_code, "name"=>$worker->name, "in_id" =>$scan->id, "in"=>Carbon::parse($scan->scan_dt)->format('g:i:s A'), "scan_in"=>$scan->scan_dt, "out_id"=>null, "out"=>"No entry", "scan_out"=>null));
                             $count++;
-                            return $data;
                         }
                         else{
                             foreach($data as &$d){
@@ -82,48 +81,54 @@ class ReportController extends Controller
 
     protected function calculateHours($records) {
 
-        $data = array();
-        $count = 0;
+        try{
+            $data = array();
+            $count = 0;
 
-        foreach($records as $record){
-            $to = \Carbon\Carbon::createFromFormat('Y-m-d H:s:i', $record['scan_in']);
-            $from = \Carbon\Carbon::createFromFormat('Y-m-d H:s:i', $record['scan_out']);
-            $diff_in_seconds = $to->diffInSeconds($from);
+            foreach($records as $record){
+                $to = \Carbon\Carbon::createFromFormat('Y-m-d H:s:i', $record['scan_in']);
+                $from = \Carbon\Carbon::createFromFormat('Y-m-d H:s:i', $record['scan_out']);
+                $diff_in_seconds = $to->diffInSeconds($from);
 
-            $hours = floor($diff_in_seconds / 3600);
-            $diff_in_seconds -= $hours * 3600;
-            $minutes = floor($diff_in_seconds / 60);
-            $hours = $hours + ($minutes/60);
+                $hours = floor($diff_in_seconds / 3600);
+                $diff_in_seconds -= $hours * 3600;
+                $minutes = floor($diff_in_seconds / 60);
+                $hours = $hours + ($minutes/60);
 
-            if($hours > 8){
-                $regular = 8;
-                $overtime = $hours - 8;
-            }
-            else{
-                $regular = $hours;
-                $overtime = 0;
-            }
+                if($hours > 8){
+                    $regular = 8;
+                    $overtime = $hours - 8;
+                }
+                else{
+                    $regular = $hours;
+                    $overtime = 0;
+                }
 
-            if(!in_array($record->worker_code, array_column($data, 'worker_code'))){
-                array_push($data, array("id"=>$count, "worker_code"=>$record->worker_code, "regular"=>$regular, "overtime"=>$overtime));
-                $count++;
-            }
-            else{
-                foreach($data as &$d){
-                    $r = $d['regular'] + $regular;
-                    if($r > 8){
-                        $d['regular'] = 8;
-                        $d['overtime'] = $d['overtime'] + ($r - 8) + $overtime;
-                    }
-                    else{
-                        $d['regular'] = $r;
-                        $d['overtime'] = $d['overtime'] + $overtime;
+                if(!in_array($record->worker_code, array_column($data, 'worker_code'))){
+                    array_push($data, array("id"=>$count, "worker_code"=>$record->worker_code, "regular"=>$regular, "overtime"=>$overtime));
+                    $count++;
+                }
+                else{
+                    foreach($data as &$d){
+                        $r = $d['regular'] + $regular;
+                        if($r > 8){
+                            $d['regular'] = 8;
+                            $d['overtime'] = $d['overtime'] + ($r - 8) + $overtime;
+                        }
+                        else{
+                            $d['regular'] = $r;
+                            $d['overtime'] = $d['overtime'] + $overtime;
+                        }
                     }
                 }
             }
-        }
 
-        return $data;
+            return $data;
+        }
+        catch (\Exception $e) {
+            Log::error("Exc caught while RecordController@calculateHours: " . $e->getMessage());
+            return null;
+        }
     }
 
 }
