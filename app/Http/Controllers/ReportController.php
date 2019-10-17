b7f687da11a51849c1c14460bda7a7cadff0baf2
misheki <kurky17@yahoo.com>
2019-10-17 14:14:17 -0700

    Test daily report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -86,44 +86,44 @@ class ReportController extends Controller
             $count = 0;
 
             foreach($records as $record){
-                return $record['worker_code'];
-                $to = \Carbon\Carbon::createFromFormat('Y-m-d H:s:i', $record['scan_in']);
-                $from = \Carbon\Carbon::createFromFormat('Y-m-d H:s:i', $record['scan_out']);
-                $diff_in_seconds = $to->diffInSeconds($from);
+                if(!is_null($record['scan_out'])){
+                    $to = \Carbon\Carbon::createFromFormat('Y-m-d H:s:i', $record['scan_in']);
+                    $from = \Carbon\Carbon::createFromFormat('Y-m-d H:s:i', $record['scan_out']);
+                    $diff_in_seconds = $to->diffInSeconds($from);
 
-                $hours = floor($diff_in_seconds / 3600);
-                $diff_in_seconds -= $hours * 3600;
-                $minutes = floor($diff_in_seconds / 60);
-                $hours = $hours + ($minutes/60);
+                    $hours = floor($diff_in_seconds / 3600);
+                    $diff_in_seconds -= $hours * 3600;
+                    $minutes = floor($diff_in_seconds / 60);
+                    $hours = $hours + ($minutes/60);
 
-                if($hours > 8){
-                    $regular = 8;
-                    $overtime = $hours - 8;
-                }
-                else{
-                    $regular = $hours;
-                    $overtime = 0;
-                }
+                    if($hours > 8){
+                        $regular = 8;
+                        $overtime = $hours - 8;
+                    }
+                    else{
+                        $regular = $hours;
+                        $overtime = 0;
+                    }
 
-                if(!in_array($record['worker_code'], array_column($data, 'worker_code'))){
-                    array_push($data, array("id"=>$count, "worker_code"=>$record['worker_code'], "regular"=>$regular, "overtime"=>$overtime));
-                    $count++;
-                }
-                else{
-                    foreach($data as &$d){
-                        $r = $d['regular'] + $regular;
-                        if($r > 8){
-                            $d['regular'] = 8;
-                            $d['overtime'] = $d['overtime'] + ($r - 8) + $overtime;
-                        }
-                        else{
-                            $d['regular'] = $r;
-                            $d['overtime'] = $d['overtime'] + $overtime;
+                    if(!in_array($record['worker_code'], array_column($data, 'worker_code'))){
+                        array_push($data, array("id"=>$count, "worker_code"=>$record['worker_code'], "regular"=>$regular, "overtime"=>$overtime));
+                        $count++;
+                    }
+                    else{
+                        foreach($data as &$d){
+                            $r = $d['regular'] + $regular;
+                            if($r > 8){
+                                $d['regular'] = 8;
+                                $d['overtime'] = $d['overtime'] + ($r - 8) + $overtime;
+                            }
+                            else{
+                                $d['regular'] = $r;
+                                $d['overtime'] = $d['overtime'] + $overtime;
+                            }
                         }
                     }
                 }
             }
-
             return $data;
         }
         catch (\Exception $e) {
